cc55e0d944b2033367241da3c1d287fa1fd2535a
lucotmo <lucotmo@gmail.com>
2019-04-24 08:54:30 -0500

    component2

---
+++ resources/js/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <div className="content">
+                <h1>About</h1>
+            </div>
+        );
+    }
+}
